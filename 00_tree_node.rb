f92a2e0758062249597b56d9313fef5148166f31
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-03-31 11:04:06 -0700

    Implement phase 3: Adding/removing children

--- 00_tree_node.rb
+++ 00_tree_node.rb
@@ -22,4 +22,18 @@ class PolyTreeNode
     @parent = node
     node.children << self unless node.nil?
   end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    if @children.include?(child_node)
+      child_node.parent = nil
+      @children.delete(child_node)
+    else
+      raise "Not a child"
+    end
+  end
+
 end
