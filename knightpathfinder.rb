1796f2bd417ac82fb09baf038e7946a9f50f19ed
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-03-31 14:13:57 -0700

    Implement Phase 1: KnightPathFinder#new_move_positions

--- knightpathfinder.rb
+++ knightpathfinder.rb
@@ -1,8 +1,51 @@
+require_relative '00_polytreenode'
+require 'byebug'
+
 class KnightPathFinder
-  def initialize(start_pos = [0,0])
+
+  KNIGHT_MOVES = [
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2],
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1]
+  ]
+
+  def self.valid_moves(pos)
+    all_moves = []
+
+    KNIGHT_MOVES.each do |move|
+      x = pos.first + move.first
+      y = pos.last + move.last
+      all_moves << [x, y] if KnightPathFinder.in_board?([x, y])
+    end
+
+    all_moves
+  end
+
+  def self.in_board?(pos)
+    pos.all? { |i| i.between?(0, 7) }
+  end
+
+  def initialize(start_pos = [0, 0])
     @start_node = PolyTreeNode.new(start_pos)
     @move_tree = self.build_move_tree
+    @visited_positions = [start_pos]
   end
+
+  def new_move_positions(pos)
+    all_moves = KnightPathFinder.valid_moves(pos)
+
+    valid_moves = all_moves.reject do |move|
+      @visited_positions.include?(move)
+    end
+
+    @visited_positions.concat(valid_moves)
+  end
+
 
   def build_move_tree
     return nil #TODO
